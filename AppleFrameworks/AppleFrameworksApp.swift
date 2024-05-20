a048fd668745c37a807e25ba4f531a91a7109c32
Maja <md.szlesinska@gmail.com>
2024-05-20 17:19:10 +0200

    Add grid with frameworks' details

--- AppleFrameworks/AppleFrameworksApp.swift
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct AppleFrameworksApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            FrameworkGridView()
         }
     }
 }
